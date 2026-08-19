263c6325f10af08083b553eed82aec7b32e51cb9
itodoconhecer-cmyk <itodoconhecer@gmail.com>
2026-08-19 20:16:00 -0300

    feat: adiciona classe Main com uso da classe Pessoa

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.nome = "João";
+        pessoa.idade = 20;
+        pessoa.tipoSanguineo = "O+";
+
+        pessoa.exibirNome();
+    }
+}
