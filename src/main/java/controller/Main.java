6f8314bc456f18fe1b11ec6717722ca8edd3a854
ItodoConhecer <itodoconhecer@gmail.com>
2026-08-25 20:16:35 -0300

    refactor: restaura Main na pasta controller

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,14 @@
+package controller;
+
+import model.Pessoa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("João", 20, "O+");
+
+        pessoa.exibirNome();
+        pessoa.exibirIdade();
+        pessoa.exibirTipoSanguineo();
+    }
+}
