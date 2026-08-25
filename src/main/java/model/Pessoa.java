6265adb23b6788b6475b493c26582e75051977ea
ItodoConhecer <itodoconhecer@gmail.com>
2026-08-25 20:16:30 -0300

    refactor: restaura Pessoa na pasta model

---
+++ src/main/java/model/Pessoa.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Pessoa {
+
+    public String nome;
+    public int idade;
+    public String tipoSanguineo;
+
+    public Pessoa(String nome, int idade, String tipoSanguineo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipoSanguineo = tipoSanguineo;
+    }
+
+    public void exibirNome() {
+        System.out.println(nome + " ola Mundo!");
+    }
+
+    public void exibirIdade() {
+        System.out.println("Idade: " + idade);
+    }
+
+    public void exibirTipoSanguineo() {
+        System.out.println("Tipo Sanguineo: " + tipoSanguineo);
+    }
+}
