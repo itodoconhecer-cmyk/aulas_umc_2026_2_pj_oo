e290b3109b9213fd350b17bed8c04e777db878b2
ItodoConhecer <itodoconhecer@gmail.com>
2026-08-25 21:21:21 -0300

    refactor: demonstra multiplos enderecos na Main

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -6,21 +6,18 @@ import model.Pessoa;
 public class Main {
 
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa("João", 20, "O+");
+        Endereco endereco1 = new Endereco("Rua Anjos", "12345", "Rio Branco");
+        Endereco endereco2 = new Endereco("Rua das Flores", "456", "Sao Paulo");
+
+        Pessoa pessoa = new Pessoa("João", 20, "O+", endereco1);
+        pessoa.adicionarEndereco(endereco2);
 
         pessoa.exibirNome();
         pessoa.exibirIdade();
         pessoa.exibirTipoSanguineo();
+        pessoa.exibirEnderecos();
 
         pessoa.fazerAniversario();
         pessoa.exibirIdade();
-
-        Endereco endereco = new Endereco();
-        endereco.setCidade("Rio Branco");
-        endereco.setNumero("12345");
-        endereco.setRua("Rua Anjos");
-
-        endereco.exibir();
-
     }
 }
